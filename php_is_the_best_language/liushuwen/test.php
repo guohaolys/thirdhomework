94f8c8a198c2778af8ecdebcf1edaffc87067d5f
ChuckGao <chuckgao9@icloud.com>
2017-04-26 21:33:31 +0800

    联调修改网页名，文件夹名，刘树文操作

--- php_is_the_best_language/liushuwen/test.php
+++ php_is_the_best_language/liushuwen/test.php
@@ -8,21 +8,22 @@ if (!$con)
 // Create table in db1 database
 mysql_select_db("db1", $con);
 
-//$sql = "CREATE TABLE user 
-//(
-//username varchar(15),
-//password varchar(15)
-//)";
-//if (!mysql_query($sql,$con))
-//  {
- // die('Error: ' . mysql_error());
- // }
+$sql = "CREATE TABLE user 
+(
+	username varchar(15),
+	password varchar(15),
+	score varchar(15)
+)";
+if (!mysql_query($sql,$con))
+  {
+ die('Error: ' . mysql_error());
+  }
 
-//$sql1="INSERT INTO user (username, password) VALUES ('1', '1')";
-//if (!mysql_query($sql1,$con))
-//  {
-//  die('Error: ' . mysql_error());
-//  }
+$sql1="INSERT INTO user (username, password) VALUES ('1', '1')";
+if (!mysql_query($sql1,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
 
 $result = mysql_query("SELECT * FROM user");
 
