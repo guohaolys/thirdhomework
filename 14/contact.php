7a54a1bd716eed197cb3b40f9677cc8bb67c0ce0
unknown <you@example.com>
2017-05-15 13:59:05 +0800

    no message

--- 14/contact.php
+++ 14/contact.php
@@ -30,116 +30,20 @@
     <script src="js/respond.min.js"></script>
 <![endif]==========-->
 </head>
-<body class="home">
-
-<header class="row transparent white" data-spy="affix" data-offset-top="300" id="header">
-    <div class="container">
-        <div class="row top-header">
-            <div class="col-sm-4 search-form-col">
-                <form action="#" method="get" class="search-form">
-                    <div class="input-group">
-                        <span class="input-group-addon"><img src="images/search-icon-dark.png" alt=""></span>
-                        <input type="search" class="form-control" placeholder="search">
-                    </div>
-                </form>
-            </div>
-            <div class="col-sm-4 logo-col text-center">
-                <a href="index.html"><img src="images/2.png" alt=""></a>
-            </div>
-            <div class="col-sm-4 menu-trigger-col">
-                <button class="menu-trigger black pull-right">
-                    <span class="active-page">Contact</span>
-                    <img src="images/menu-align-dark.png" alt="" class="icon-burger">
-                    <img src="images/menu-close-dark.png" alt="" class="icon-cross">
-                </button>
-            </div>
-        </div>        
-    </div>
-    <div class="row menu-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-8 menu-col">
-                    <div class="row">
-                        <ul class="nav column-menu white-bg">
-                                <li><a href="#">Home</a>
-                            </li>
-                            <li><a href="about.html">About</a></li>
-                            <li><a href="single.html">Blog Single 1</a></li>
-                            <li><a href="single2.html">Blog Single 2</a></li>
-                        </ul>
-                        <ul class="nav column-menu white-bg">
-                            <li><a href="single3.html">Blog Single 3</a></li>
-                            <li><a href="single3.html">Blog Single 4</a></li>
-                            <li class="active"><a href="contact.html">contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div>
-	
-</header>
 
 <?php
 $name = $email = $gender = $comment = $website = "";
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
-$error_count=0;
 $bingo_recording=0;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-   if (empty($_POST["name"])) {
-     $nameErr = "姓名是必填的";
-	 $error_count++;
-	 echo"1";
-   } else {
-     $name = test_input($_POST["name"]);   
-   }
 
-
-   if (empty($_POST["email"])) {
-     $emailErr = "电邮是必填的";
-	  $error_count++;
-	  echo"2";
-   } else {
-     $email = test_input($_POST["email"]);
-     // 检查电子邮件地址语法是否有效
-     if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)) {
-       $emailErr = "无效的 email 格式";
-	    $error_count++;
-     }
-   }
-
-   if (empty($_POST["website"])) {
-     $website = "";
-	 echo"3";
-   } else {
-     $website = test_input($_POST["website"]);
-     // 检查 URL 地址语法是否有效（正则表达式也允许 URL 中的斜杠）
-     if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-       $websiteErr = "无效的 URL";
-	    $error_count++;
-     }
-   }
-
-   if (empty($_POST["comment"])) {
-     $comment = "";
-	 echo"4";
-   } else {
-     $comment = test_input($_POST["comment"]);
-   }
-
-   if (empty($_POST["gender"])) {
-     $genderErr = "性别是必填的";
-	  $error_count++;
-	  echo"5";
-   } else {
-     $gender = test_input($_POST["gender"]);
-   }
+     @$name = test_input($_POST['name']);   
+     @$email = test_input($_POST["email"]);
+     @$website = test_input($_POST["website"]);
+     @$comment = test_input($_POST["comment"]);
+     @$gender = test_input($_POST["gender"]);
    
-   
-   if($error_count==0)
-   {
  		$link = mysql_connect ( 'localhost', 'root', '123' ) or die ( 'Could not connect: ' . mysql_error () );
 		if (mysql_query("CREATE DATABASE IF NOT EXISTS contact CHARACTER SET utf8 COLLATE utf8_general_ci",$link))
 		  {
@@ -183,7 +87,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		  $bingo_recording++;
 		  }
 		mysql_close ( $link );
-   }
+  
 }
 
 
@@ -195,6 +99,57 @@ function test_input($data) {
 }
    
 ?>
+
+<body class="home">
+
+<header class="row transparent white" data-spy="affix" data-offset-top="300" id="header">
+    <div class="container">
+        <div class="row top-header">
+            <div class="col-sm-4 search-form-col">
+                <form action="#" method="get" class="search-form">
+                    <div class="input-group">
+                        <span class="input-group-addon"><img src="images/search-icon-dark.png" alt=""></span>
+                        <input type="search" class="form-control" placeholder="search">
+                    </div>
+                </form>
+            </div>
+            <div class="col-sm-4 logo-col text-center">
+                <a href="index.html"><img src="images/2.png" alt=""></a>
+            </div>
+            <div class="col-sm-4 menu-trigger-col">
+                <button class="menu-trigger black pull-right">
+                    <span class="active-page">Contact</span>
+                    <img src="images/menu-align-dark.png" alt="" class="icon-burger">
+                    <img src="images/menu-close-dark.png" alt="" class="icon-cross">
+                </button>
+            </div>
+        </div>        
+    </div>
+    <div class="row menu-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-8 menu-col">
+                    <div class="row">
+                        <ul class="nav column-menu white-bg">
+                            <li><a href="index.html" >Home</a></li>
+							<li><a href="yuanyu3.html">儿童经典读物</a></li>
+                            <li><a href="../final_wxr.html">三毛&echo </a></li>
+                        </ul>
+                        <ul class="nav column-menu white-bg">
+                            <li><a href="ZHANGY/zy.html">进口图书</a></li>
+                            <li><a href="xingchang.html">心理学图书</a></li>                                                    
+                            <li class="active"><a href="contact.php">contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+	
+</header>
+
+
 <section class="row page-content-wrap">
     <div class="container">
         <h2 class="page-title text-center">contact</h2>
@@ -202,44 +157,51 @@ function test_input($data) {
             <div class="col-md-8 page-contents">
                 <div class="row page-content">
                     <div class="contents row">
-                        <h4>Mark Samder, lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</h4>
+                        <h4></h4>
                     
-                        <p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <!--<p>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>-->
                         <br>
 						<div style="margin-top:10px;" id="formarea">
-                        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post" class="contact-form row">
+						
+						<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+
                             <div class="form-group col-sm-6">
                                 <label for="name">full name*</label>
-                                <input type="text" id="name" class="form-control" placeholder="Your name" required>
+								<input type="text" name="name" class="form-control" placeholder="Your name" required>
+								<span class="error"><?php echo $nameErr;?></span>
                             </div>
                             <div class="form-group col-sm-6">
                                 <label for="email">Email*</label>
-                                <input type="email" id="email" class="form-control" placeholder="Your email address here" required>
+								<input type="email" name="email" class="form-control" placeholder="Your email address here" required>
+								<span class="error"><?php echo $emailErr;?></span>
                             </div>
                             <div class="form-group col-sm-6">
                                 <label for="gender">gender</label>
-                                <input type="text" id="gender" class="form-control" placeholder="Your gender">
+                                <input type="text" name="gender" class="form-control" placeholder="Your gender">
+								<span class="error"><?php echo $genderErr;?></span>
                             </div>
                             <div class="form-group col-sm-6">
                                 <label for="website">website</label>
-                                <input type="url" id="website" class="form-control" placeholder="Your website url">
+                                <input type="url" name="website" class="form-control" placeholder="Your website url">
+								
                             </div>
                             <div class="form-group col-sm-12">
                                 <label for="comment">comment</label>
-                                <textarea name="comment" id="comment" class="form-control" placeholder="Write comment here"></textarea>
+                                <textarea name="comment" class="form-control" placeholder="Write comment here" required></textarea>
                             </div>
                             <div class="col-sm-12">
-                                <!--<button type="submit" class="btn-primary"><span>send</span></button>-->
-								<input type="submit" name="submit" value="send">
+                                <button type="submit" class="btn-primary"><span>send</span></button>
                                 <h5 class="pull-right">*required fields</h5>
                             </div>
                         </form>
+						</br>
+						</br>
+						 <div>
 						<?php
-						if($bingo_recording>0||$error_count>0) 
-							echo "抱歉未能够正常保持您的留言，请重新输入!";
-						if($_SERVER["REQUEST_METHOD"] == "POST"&&$error_count==0&&$bingo_recording==0)
-							echo "很高兴能够收到您的留言!";
+						if($_SERVER["REQUEST_METHOD"] == "POST"&&$bingo_recording==0)
+							echo "<h4>很高兴能够收到您的留言!</h4>";
 				        ?>
+						</div>
 						</div>
                     </div>                    
                 </div>
@@ -248,11 +210,11 @@ function test_input($data) {
                 <!--Author Widget-->
                 <aside class="row m0 widget-author widget has-pt">
                     <div class="widget-author-inner row">
-                        <div class="author-avatar row"><a href="#"><img src="images/author.png" alt="" class="img-circle"></a></div>
-                        <a href="#"><h2 class="author-name">Mark Sanders</h2></a>
-                        <h5 class="author-title">small title</h5>
-                        <p>The word nature is derived from the Latin word natura, or "essential qualities, innate disposition", and in ancient times, literally meant "birth".</p>
-                        <a href="#" class="know-more">know more</a>
+                        <div class="author-avatar row"><img src="images/bear41.png" alt="" class="img-circle"></div>
+                        <a href="#"><h2 class="author-name">Join Us</h2></a>
+                        <h5 class="author-title">14**</h5>
+                        <p>SHARE &nbsp THE &nbsp BOOK &nbsp YOU &nbsp LIKE &nbsp WITH &nbsp US</p>
+                        <p class="know-more">more</p>
                     </div>
                     <ul class="nav social-nav">
                         <li><a href="#"><i class="fa fa-twitter"></i></a></li>
@@ -266,23 +228,21 @@ function test_input($data) {
         </div>
     </div>
 	
-	<div style="height:250px;border:#ccc solid 1px;  margin: auto;" id="dituContent"></div>
-		
+<div class="container">
+       <h2 class="page-title text-center">history</h2>		
 		<?php
 		$link = mysql_connect ( 'localhost', 'root', '123' ) or die ( 'Could not connect: ' . mysql_error () );///////////////////////////////////////
 		//echo 'Connected successfully';
-		//CREATE DATABASE html131class CHARACTER SET utf8 COLLATE utf8_general_ci;
 		if (mysql_query("CREATE DATABASE IF NOT EXISTS contact CHARACTER SET utf8 COLLATE utf8_general_ci",$link))
 		  {
-		 // echo "Database html131class created sentence run smoothly";
 		  }
 		else
 		  {
 		  echo "Error creating database: " . mysql_error();
 		  }
 
-		 mysql_select_db("contact",$link);
-		 $sql_table="CREATE TABLE IF NOT EXISTS `visitor` (";
+		mysql_select_db("contact",$link);
+		$sql_table="CREATE TABLE IF NOT EXISTS `visitor` (";
 		$sql_table.="`id`  int NOT NULL AUTO_INCREMENT ,";
 		$sql_table.="`name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,";
 		$sql_table.="`email`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL ,";
@@ -303,7 +263,7 @@ function test_input($data) {
 		echo "<div id=\"history_records\">";
 		echo "<table id=\"history_records_table\" border='1'>
 		<tr>
-		<th>ID</th>
+		<th><h4>ID</h4></th>
 		<th>姓名</th>
 		<th>邮箱</th>
 		<th>评论</th>
@@ -325,8 +285,6 @@ function test_input($data) {
 		echo "</table>";
 		echo "</div>";
 		?>
-	</div>
-
 
 </div>
 <script src="js/sharedfunctions.js" type="text/javascript" ></script>
@@ -343,15 +301,14 @@ function test_input($data) {
             <div class="widget col-sm-5 widget-menu">
                 <div class="row">
                     <ul class="nav column-menu white-bg">
-                        <li class="active"><a href="index.html">Home</a></li>
-                        <li><a href="about.html">About</a></li>
-                        <li><a href="single.html">Blog Single 1</a></li>
-                        <li><a href="single2.html">Blog Single 2</a></li>
-                    </ul>
-                    <ul class="nav column-menu white-bg">
-                        <li><a href="single3.html">Blog Single 3</a></li>
-                        <li><a href="single3.html">Blog Single 4</a></li>
-                        <li><a href="contact.html">contact</a></li>
+                        <li><a href="index.html" >Home</a></li>
+							<li><a href="yuanyu3.html">儿童经典读物</a></li>
+                            <li><a href="../final_wxr.html">三毛&echo </a></li>
+                        </ul>
+                        <ul class="nav column-menu white-bg">
+                            <li><a href="ZHANGY/zy.html">进口图书</a></li>
+                            <li><a href="xingchang.html">心理学图书</a></li>
+                            <li class="active"><a href="contact.html">contact</a></li>
                     </ul>
                 </div>
             </div>
